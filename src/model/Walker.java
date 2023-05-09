5e6a3b3b2771c2cb4d8cc8e7bba45d27c23430af
tada195908 <48470775+tada195908@users.noreply.github.com>
2023-05-09 10:03:36 +0900

    Add files via upload

--- src/model/Walker.java
+++ src/model/Walker.java
@@ -1,1 +1,52 @@
-package model;/** * Walker class * * @author tadaki */public class Walker {    protected int x;//position of the walker    protected final double p;//probability for moving right    /**     * @param x initial position     */    public Walker(int x) {        this(x, 0.5);//probability for moving right is .5    }    /**     * @param x initial potision     * @param p probability for moving right     */    public Walker(int x, double p) {        this.x = x;        this.p = p;    }    /**     * one step     *     * @return new position     */    public int walk() {        double r = Math.random();        if (r < p) {//x++ with probability p            x++;        } else {//x-- with probability 1-p            x--;        }        return x;    }    /**     * return current position     *     * @return     */    public int getX() {        return x;    }}
+package model;
+
+/**
+ * Walker class
+ *
+ * @author tadaki
+ */
+public class Walker {
+
+    protected int x;//position of the walker
+    protected final double p;//probability for moving right
+
+    /**
+     * @param x initial position
+     */
+    public Walker(int x) {
+        this(x, 0.5);//probability for moving right is .5
+    }
+
+    /**
+     * @param x initial position
+     * @param p probability for moving right
+     */
+    public Walker(int x, double p) {
+        this.x = x;
+        this.p = p;
+    }
+
+    /**
+     * one step
+     *
+     * @return new position
+     */
+    public int walk() {
+        double r = Math.random();
+        if (r < p) {//x++ with probability p
+            x++;
+        } else {//x-- with probability 1-p
+            x--;
+        }
+        return x;
+    }
+
+    /**
+     * return current position
+     *
+     * @return
+     */
+    public int getX() {
+        return x;
+    }
+}
